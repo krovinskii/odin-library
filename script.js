eb9dd1cec05ff4d7b2e9c56b503cf0a141eb4141
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2025-01-04 16:34:12 -0600

    Basic functionality, user can add books but with no checks

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const addBookButton = document.getElementById("userInputBtn");
-const userInputHTML = document.getElementById("userInput");
-
 document.addEventListener("DOMContentLoaded", function() {
     const dialog = document.querySelector("#userBookInput-dialog");
     const btnOpen = document.querySelector(".open-modal");
@@ -9,20 +6,75 @@ document.addEventListener("DOMContentLoaded", function() {
         dialog.showModal();
     });
 });
+const addBookButton = document.getElementById("userInputBtn");
+const userInputHTML = document.getElementById("userInput");
+const formSubmit = document.getElementById("submit");
+const myLibrary = [];
 
-const userLibrary = [];
-
-function Book(title, author, rating, pages) {
+function Book(title, author, rating, pages, yesno) {
     this.title = title;
     this.author = author;
     this.rating = rating;
     this.pages = pages;
-};
+    this.yesno = yesno;
+    }
+    
+//get user input
+const userValues = document.getElementById("submitBtn").onclick = () => {
+    let title = document.getElementById("bookTitle").value;
+    let author = document.getElementById("bookAuthor").value;
+    let rating = document.getElementById("bookRating").value;
+    let pages = document.getElementById("bookPages").value;
+    let yesno = document.getElementsByName("yesno").value;
+    console.log(title + author + rating + pages + yesno);
+    
+    const userBook = new Book(title, author, rating, pages, yesno);
+    myLibrary.push(userBook);
+    console.log(myLibrary);
+    addBookToLibrary();
+    return myLibrary;
+}
+const addBookToLibrary = () => {
+    for (let i = myLibrary.length - 1; i < myLibrary.length; i++) {
+        const container = document.getElementById("userBooks");
 
-function addBookToLibrary() {
-    const titleInput = document.getElementById("#bookTitle");
-    const authorInput = document.getElementById("bookAuthor");
-    const ratingInput = document.getElementById("#bookRating");
-    const pagesInput = document.getElementById("#bookPages");
-    const readInput = document.getElementsByName("yesno");
-};
+        const newTitleContainer = document.createElement("div");
+        newTitleContainer.classList.add("titleContainer", "userInput")
+
+        const newTitle = document.createElement("div");
+        newTitle.textContent = myLibrary[i].title;
+        newTitle.classList.add("title");
+
+        const newAuthor = document.createElement("div");
+        newAuthor.textContent = myLibrary[i].author;
+        newAuthor.classList.add("author");
+
+        const newRating = document.createElement("div");
+        newRating.textContent = `${myLibrary[i].rating}/ 5`;
+        newRating.classList.add("rating", "userInput")
+
+        const newPages = document.createElement("div");
+        newPages.textContent = myLibrary[i].pages;
+        newPages.classList.add("pages", "userInput")
+
+        const newRead = document.createElement("div");
+        newRead.textContent = myLibrary[i].read;
+        newRead.classList.add("read", "userInput")
+
+        container.appendChild(newTitleContainer);
+        newTitleContainer.appendChild(newTitle);
+        newTitleContainer.appendChild(newAuthor);
+        container.appendChild(newRating);
+        container.appendChild(newPages);
+        container.appendChild(newRead);
+
+
+
+    }
+
+}
+
+
+
+
+////Need to add "Read?" functionality. Need to add checks for characters, number length, etc.
